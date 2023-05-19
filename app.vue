d06dbeac2d8b97e5582964655de6217f18c17238
chaitanya8080 <girase.chaitanya@gmail.com>
2023-05-19 18:37:05 +0530

    nuxt 3 tut intro

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  <Counter/> 
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <!-- <NuxtWelcome /> -->
+      <h1>hellow </h1>
+      <button @click="increment()">Increment</button>
+      <h2>{{ count }}</h2>
+      <button @click="decrement()">Decrement</button>
+    </div>
+  </template>
+  
+  
+  <script setup>
+  
+  const count = ref(0)
+  
+  const increment = ()=>{
+    count.value++
+  }
+  const decrement = ()=>{
+    count.value--
+  }
+  
+  </script>
+  
